49c2c8a910d6718064936388ed504fa5a9f15d58
Tim La Roche <timroche@expediagroup.com>
2022-12-04 10:40:37 +0000

    day3

--- day3/part1.scala
+++ day3/part1.scala
@@ -3,8 +3,22 @@ import scala.io.Source
 object PartOne {
 	def main(args: Array[String]) = {
 		val inputPath = args(0)
+		val alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		var sum = 0
 		for (line <- Source.fromFile(inputPath).getLines) {
-			
+			val chars = line.toCharArray()
+			val length = chars.length
+			//val compartmentOne = chars.slice(0,(length/2))
+			//val compartmentTwo = chars.slice((length/2), length)
+			val compartmentOneSet = chars.slice(0,length/2).toSet
+			val compartmentTwoSet = chars.slice((length/2), length).toSet
+			val overlap = compartmentOneSet & compartmentTwoSet
+			val x: Char = overlap.head
+			sum = sum + (alphabet.indexOf(x)+1)
+			//println(s"sack: ${line} ;; length: ${length} ;; ompartmentOne: ${compartmentOne.length} ;; compartmentTwo: ${compartmentTwo.length} ;; overlap: ${overlap}")
 		}
+		println(sum)
 	}
 }
+// todo learnings: how are sets represented in comp sci?
+
--- day3/part2.scala
+++ day3/part2.scala
@@ -1,4 +1,22 @@
 import scala.io.Source
 
 object PartTwo {
+	def main(args: Array[String]) = {
+		val inputPath = args(0)
+		val alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		var sum = 0
+		for (line <- Source.fromFile(inputPath).getLines.grouped(3)) {
+			val bagOne = line(0)
+			val bagTwo = line(1)
+			val bagThree = line(2)
+			val bagOneSet = bagOne.toSet
+			val bagTwoSet = bagTwo.toSet
+			val bagThreeSet = bagThree.toSet
+			val overlap = bagOneSet & bagTwoSet & bagThreeSet
+			val x: Char = overlap.head
+			println(x)
+			sum = sum + (alphabet.indexOf(x)+1)
+		}
+		println(sum)
+	}
 }
